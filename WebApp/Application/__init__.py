f954ccd6e3269b043d0b33b3be1c3bfb90c358d3
Nicholas Whyte <nick@nickwhyte.com>
2014-09-02 15:20:48 +1000

    Better Examples & Gitignore for datastore

--- WebApp/Application/__init__.py
+++ WebApp/Application/__init__.py
@@ -58,25 +58,24 @@ mail = Mail(app)
 
 
 def setup():
-    db.create_all()
-    if (len(User.query.filter_by(email='admin@localhost').all()) == 0):
-        # No user exists, create one.
-        user_datastore.create_role(name="admin", description="Admin Users")
-        user_datastore.create_user(email='admin@localhost', password=encrypt_password('admin'))
-        db.session.commit()
+    with app.app_context():
+        db.create_all()
 
-        user = user_datastore.find_user(email='admin@localhost')
-        role = user_datastore.find_role('admin')
-        user_datastore.add_role_to_user(user, role)
+        if (len(User.query.filter_by(email='admin@localhost').all()) == 0):
+            if len(Role.query.filter_by(name='admin').all()) == 0:
+                # No user exists, create one.
+                user_datastore.create_role(name="admin", description="Admin Users")
+                user_datastore.create_user(email='admin@localhost', password=encrypt_password('admin'))
+                db.session.commit()
 
-        db.session.commit()
+                user = user_datastore.find_user(email='admin@localhost')
+                role = user_datastore.find_role('admin')
+                user_datastore.add_role_to_user(user, role)
+
+                db.session.commit()
 
 
-# # Create a user to test with
-@app.before_first_request
-def create_user():
-    setup()
-    pass
+setup()
     
 
 # ---------------------
