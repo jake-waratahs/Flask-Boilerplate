e57152f7ef40edd327c7a6e6687396b0fb8bea93
Nicholas Whyte <nick@nickwhyte.com>
2014-09-02 14:25:49 +1000

    Better python

--- WebApp/Application/__init__.py
+++ WebApp/Application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 # Get flask Resful for the API usage
 from flask.ext import restful
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+from flask.ext.security import Security, SQLAlchemyUserDatastore, login_required
 from flask.ext.security.utils import encrypt_password
 from flask_mail import Mail
 
@@ -59,15 +59,15 @@ mail = Mail(app)
 
 def setup():
     db.create_all()
-    if (len(User.query.filter_by(email='root@localhost').all()) == 0):
+    if (len(User.query.filter_by(email='admin@localhost').all()) == 0):
         # No user exists, create one.
         user_datastore.create_role(name="admin", description="Admin Users")
-        user_datastore.create_user(email='root@localhost', password=encrypt_password('toor'))
+        user_datastore.create_user(email='admin@localhost', password=encrypt_password('admin'))
         db.session.commit()
 
-        user = user_datastore.find_user(email='root@localhost')
+        user = user_datastore.find_user(email='admin@localhost')
         role = user_datastore.find_role('admin')
-        result = user_datastore.add_role_to_user(user, role)
+        user_datastore.add_role_to_user(user, role)
 
         db.session.commit()
 
@@ -86,7 +86,7 @@ ADMINS = ['admin@localhost']
 if not app.debug:
     import logging
     from logging.handlers import SMTPHandler
-    mail_handler = SMTPHandler('127.0.0.1', 'server-error@localhost.com', ADMINS, 'Cladmix Application Failed')
+    mail_handler = SMTPHandler('127.0.0.1', 'server-error@localhost.com', ADMINS, 'Application Failed')
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
