d08bf08b592c3b489d6a9a9395eb6495a1a9c1a5
Nicholas Whyte <nick@nickwhyte.com>
2014-12-03 23:07:19 +1100

    Okay this should work...

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ cd WebApp
 # Use the sample config if one doesn't exist in the project
 if [ ! -f Application/config/variables.py ]
 	then
-	cp Application/config/variables.py.example WebApp/Application/config/variables.py
+	cp Application/config/variables.py.example Application/config/variables.py
 fi
 
 make test &&
