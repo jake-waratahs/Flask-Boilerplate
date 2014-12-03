3771e0e6563e6ce0b4d4d3f6c2a6b4423f928914
Nicholas Whyte <nick@nickwhyte.com>
2014-12-03 23:20:18 +1100

    Re-added the variables to the repo, configuration now is a little more universal

--- ci.sh
+++ ci.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # Script for running on a CI Server
 cd WebApp
-
-# Use the sample config if one doesn't exist in the project
-if [ ! -f Application/config/variables.py ]
-	then
-	cp Application/config/variables.py.example Application/config/variables.py
-fi
-
-make test &&
-rm -rf Application/config/variables.py
+make test
