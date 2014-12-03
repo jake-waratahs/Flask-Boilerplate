6ce677c9445bb0c9346a7bb93a73365a0cc49007
Nicholas Whyte <nick@nickwhyte.com>
2014-12-03 23:03:14 +1100

    CI Runs without Variables file

--- ci.sh
+++ ci.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Script for running on a CI Server
 cd WebApp
+
+# Use the sample config if one doesn't exist in the project
+if [ ! -f WebApp/Application/config/variables.py ]
+	then
+	cp WebApp/Application/config/variables.py.example WebApp/Application/config/variables.py
+fi
+
 make test
+rm -rf WebApp/Application/config/variables.py
