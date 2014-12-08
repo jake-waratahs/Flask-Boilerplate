a25ec600a3982a62943f6500361735d124400a7a
Nicholas Whyte <nick@nickwhyte.com>
2014-12-08 21:23:34 +1100

    0.9.0 pre-release

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'Flask-Boilerplate',
   package_data={'Flask-Boilerplate':'Flask-Boilerplate'},
-  version = '0.1',
+  version = '0.9.0',
   description = 'A Boilerplate for flask with a bunch of utilities',
   author = 'Nick Whyte',
   author_email = 'nick@nickwhyte.com',
   url = 'https://github.com/nickw444/flask-boilerplate', # use the URL to the github repo
-  download_url = 'https://github.com/nickw444/flask-boilerplate/tarball/0.1',
+  download_url = 'https://github.com/nickw444/flask-boilerplate/tarball/0.9.0',
   keywords = ['flask', 'boilerplate', 'utility'], 
   classifiers = [],
   scripts=['scripts/foiler'],
