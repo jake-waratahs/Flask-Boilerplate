986ab233c933ae1dd7673966dfcc9427ab1ff68b
Nicholas Whyte <nick@nickwhyte.com>
2014-12-08 23:16:26 +1100

    Clear den pyx files, lots of dists

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from distutils.core import setup
+import os 
+
+p = os.path.join(os.path.dirname(os.path.realpath(__file__)), './flask_boilerplate/boilerplate/')
+replace = os.path.join(os.path.dirname(os.path.realpath(__file__)), './flask_boilerplate/')
+p = os.path.realpath(p)
+replace = os.path.realpath(replace) + '/'
+
+
+data = []
+
+for dirname, dirnames, filenames in os.walk(p):
+    for filename in filenames:
+        f = os.path.join(dirname, filename)
+        if not f.endswith('.pyc'):
+          data.append(f.replace(replace, ''))
 
 
 setup(
   name = 'Flask-Boilerplate',
-  packages=['flask_boilerplate'],
-  package_data={'Flask-Boilerplate':'flask_boilerplate/boilerplate'},
-  version = '0.9.4',
+  packages = ['flask_boilerplate'],
+  package_data={'flask_boilerplate':
+    data},
+  version = '0.9.24',
   description = 'A Boilerplate for flask with a bunch of utilities',
   author = 'Nick Whyte',
   author_email = 'nick@nickwhyte.com',
