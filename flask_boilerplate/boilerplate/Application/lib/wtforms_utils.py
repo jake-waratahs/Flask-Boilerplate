e528cdcbcbb428cd911e75eb6efc06c36db29157
Nicholas Whyte <nick@nickwhyte.com>
2014-12-09 12:23:52 +1100

    Email validator and improvements to wtforms render field

--- flask_boilerplate/boilerplate/Application/lib/wtforms_utils.py
+++ flask_boilerplate/boilerplate/Application/lib/wtforms_utils.py
@@ -1,6 +1,6 @@
-from wtforms.validators import Optional, ValidationError, DataRequired
+from wtforms.validators import Optional, ValidationError
 import os
-
+from Application.models.User import User
 
 class ValidFileFormat(Optional):
     def __init__(self, fileupload, *args, **kwargs):
@@ -13,3 +13,7 @@ class ValidFileFormat(Optional):
     	filename, extension = os.path.splitext(field.data.filename)
     	if not self.fileupload.extension_allowed(extension[1:]):
     		raise ValidationError(self.message)
+
+def email_available(form, field):
+        if User.query.filter(User.email == field.data).first():
+            raise ValidationError('A user with this email address already exists, email addresses must be unique.')
