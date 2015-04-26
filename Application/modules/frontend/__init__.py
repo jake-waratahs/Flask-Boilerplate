a69f0dacffeec139ed63c04f99a8e94fec76913c
Nicholas Whyte <nick@nickwhyte.com>
2015-04-26 23:41:12 +1000

    Start writing cleanup

--- Application/modules/frontend/__init__.py
+++ Application/modules/frontend/__init__.py
@@ -8,11 +8,11 @@ from .controllers.Index import Index
 Index.register(frontend)
 menu.register_flaskview(frontend, Index)
 
-
+# Pragma - Submodule Registration Start
+# Anything after the above line will be removed after cleanup.
 from .modules.learn_flask import learn_flask
 from .modules.examples import examples
+from .modules.cleanup import cleanup
 frontend.register_blueprint(learn_flask, url_prefix='/learn-flask')
 frontend.register_blueprint(examples, url_prefix='/examples')
-
-
-
+frontend.register_blueprint(cleanup, url_prefix='/cleanup')
--- Application/modules/frontend/modules/cleanup/__init__.py
+++ Application/modules/frontend/modules/cleanup/__init__.py
@@ -1,3 +1,45 @@
 """
 A Module to clean up the boilerplate and delete everything. 
 """
+
+from flask import Blueprint
+
+
+cleanup = Blueprint('frontend.cleanup', __name__, template_folder="templates", 
+    static_folder="static")
+
+from flask.ext.classy import FlaskView, route
+from flask.ext import menu
+from flask import render_template, request, redirect
+
+
+class Cleanup(FlaskView):
+    route_base = '/'
+
+    @route('/', methods=['GET','POST'])
+    @menu.classy_menu_item('frontend.cleanup', 'Cleanup', order=4)
+    def index(self):
+        if request.method == 'POST':
+            print(" * Cleaning up...")
+            print(" * Unlinking Submodules")
+            kept_lines = None
+            with open('./Application/modules/frontend/__init__.py', 'r') as fh:
+                kept_lines = fh.readlines()
+
+            with open('./Application/modules/frontend/__init__.py', 'w') as fh:
+                for line in kept_lines:
+                    if "Pragma - Submodule Registration Start" in line:
+                        break
+
+                    fh.write(line)
+
+            print(" * Deleting Submodules")
+            print(" * Deleting Models")
+            print(" * Cleanup Complete!")
+
+            # return redirect('/')
+
+        return render_template('frontend.cleanup/index.html')
+
+Cleanup.register(cleanup)
+menu.register_flaskview(cleanup, Cleanup)
